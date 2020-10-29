6d1311bdafd2b6fe250600e6d8208439f57980b1
Elmar Wouters <elmar@elmarwouters.nl>
2020-10-29 21:31:52 +0100

    Everything to Typescript

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,15 +2,17 @@ import Head from 'next/head'
 import Layout from '../components/layout'
 
 // pages/404.js
-export default function Custom404() {
+function Custom404() {
     return (
         <Layout>
-        <Head>
-            <title>404 - Page Not Found</title>
-        </Head>
+            <Head>
+                <title>404 - Page Not Found</title>
+            </Head>
             <article>
                 <h1>404 - Page Not Found</h1>
             </article>
         </Layout>
     )
 }
+
+export default Custom404
