e0abc076d8a0b815efa8bf6406acd792f3ed98b8
Elmar Wouters <elmar@elmarwouters.nl>
2020-10-29 20:43:36 +0100

    Changed everything to scss again

---
+++ pages/404.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Layout from '../components/layout'
+
+// pages/404.js
+export default function Custom404() {
+    return (
+        <Layout>
+        <Head>
+            <title>404 - Page Not Found</title>
+        </Head>
+            <article>
+                <h1>404 - Page Not Found</h1>
+            </article>
+        </Layout>
+    )
+}
